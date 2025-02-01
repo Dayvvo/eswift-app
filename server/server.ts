44f286f0f8b7165660fe98e4055eb3e50488e2d6
dayvvo <dyvvoo@gmail.com>
2025-02-01 20:01:04 +0100

    fix: switch to port 3000

--- server/server.ts
+++ server/server.ts
@@ -114,7 +114,7 @@ nextApp.prepare().then(() => {
     })
   );
 
-  const PORT = process.env.PORT || 5500;
+  const PORT = process.env.PORT || 3000;
 
   app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 })
