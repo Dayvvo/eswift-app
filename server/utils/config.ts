e992af03ac22ff7b0b070c823f00436f283fc528
dayvvo <dyvvoo@gmail.com>
2025-03-07 22:19:54 +0100

    fix: add google redirect uri

--- server/utils/config.ts
+++ server/utils/config.ts
@@ -26,7 +26,7 @@ export class appConfig {
         {
           clientID: process.env['GOOGLE_CLIENT_ID'] as string,
           clientSecret: process.env['GOOGLE_CLIENT_SECRET'] as string,
-          callbackURL: `${process.env['BACKEND_URL']}/api/auth/google/callback`,
+          callbackURL: `${process.env['BACKEND_URL']}/auth/google/callback`,
           proxy: true,
           passReqToCallback: true,
         },
