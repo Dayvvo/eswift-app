e024e3b2d4dcf4e28e936690fbc938d6a3b39f36
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2025-05-02 01:05:58 +0100

    listing instead of property

---
+++ pages/listing/[id].tsx
@@ -0,0 +1,13 @@
+// import { PropertyDetails } from "@/components/PropertyDetails";
+import Wrapper from "../../components/Wrapper";
+import { PropertyDetails } from "../../screens/Property/propertyDetails";
+
+const PropertyDetailScreen = () => {
+  return (
+    <Wrapper>
+      <PropertyDetails />
+    </Wrapper>
+  );
+};
+
+export default PropertyDetailScreen;
---
+++ pages/listing/index.tsx
@@ -0,0 +1,13 @@
+import Wrapper from "@/components/Wrapper";
+import { PropertyScreen } from "../../screens/Property/property";
+
+
+const Property =()=> {
+    return (
+        <Wrapper noPadding>
+            <PropertyScreen/>
+        </Wrapper>
+    )
+}
+
+export default Property;
