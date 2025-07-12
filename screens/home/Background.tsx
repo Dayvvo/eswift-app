5835e383b43e537d2e1d475150a5f421954aa35e
ogedengbewisdom <ogedengbewisdom123456@gmail.com>
2025-07-12 04:16:49 +0100

    fix: quick fix

--- screens/home/Background.tsx
+++ screens/home/Background.tsx
@@ -1,28 +1,24 @@
 import { Box, Flex, Image } from "@chakra-ui/react";
 
-export const Background =()=> {
-    return(
-
-        <Box
-            w={'100%'} h={'100vh'}
-            bgImage="url('/BG2.png')"
-            bgSize="cover"
-            bgPosition="center"
-            className="robotoF"
-            position={'fixed'}
-            left={0}
-            top={0}
-            overflow={'hidden'}
-            zIndex={-1}
-        >
-            <Flex w={'100%'}
-                justifyContent={'space-between'}
-                mt={12}
-            >
-                <Image src="/coconutLeaf5.png" alt="coco" w="800px" h="500px"/>
-                <Image src="/treeLeaf1.png" alt="tree" w="800px" h="500px"/>
-            </Flex>
-        </Box>
-
-    )
+export const Background = () => {
+  return (
+    <Box
+      w={"100%"}
+      h={"100vh"}
+      bgImage="url('/BG2.png')"
+      bgSize="cover"
+      bgPosition="center"
+      className="robotoF"
+      position={"fixed"}
+      left={0}
+      top={0}
+      overflow={"hidden"}
+      zIndex={-1}
+    >
+      <Flex w={"100%"} justifyContent={"space-between"} mt={12}>
+        <Image src="/coconutLeaf5.png" alt="coco" w="800px" h="500px" />
+        <Image src="/treeLeaf1.png" alt="tree" w="800px" h="500px" />
+      </Flex>
+    </Box>
+  );
 };
